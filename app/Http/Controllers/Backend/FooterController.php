989a84756e0031849fc29ca17ad9d515deda80e7
mohammad Almamoon <almamoon604@gmail.com>
2022-09-10 01:10:03 +0600

    successfully Stored

--- app/Http/Controllers/Backend/FooterController.php
+++ app/Http/Controllers/Backend/FooterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Backend\Foter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class FooterController extends Controller
 {
@@ -35,7 +37,43 @@ class FooterController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+           'number' =>'required|numeric|min:14', 
+           'footer_short_description' =>'required', 
+           'address' =>'required', 
+           'email' =>'required|unique:foters', 
+           'facebook_link' =>'required', 
+           'twitter_link' =>'required', 
+           'linkedin_link' =>'required', 
+           'dribble_link' =>'required', 
+           'pinterest_link' =>'required', 
+           'copywrite_text' =>'required', 
+        ],[
+            'number.required' =>'Number Is Required', 
+           'footer_short_description.required' =>'Description Is required', 
+           'address.required' =>'Address is Required', 
+           'email.required' =>'Email Is Required', 
+           'facebook_link.required' =>'Facebook Is Required', 
+           'twitter_link.required' =>'Twitter Is Required', 
+           'linkedin_link.required' =>'Linkedin Required', 
+           'dribble_link.required' =>'Dribble Required', 
+           'pinterest_link.required' =>'Pinterest Required', 
+           'copywrite_text.required' =>'CopyWrite Text Required', 
+        ]);
+        Foter::insert([
+            'number'=> $request->number,
+            'footer_short_description'=> $request->footer_short_description,
+            'address'=> $request->address,
+            'email'=> $request->email,
+            'facebook_link'=> $request->facebook_link,
+            'twitter_link'=> $request->twitter_link,
+            'linkedin_link'=> $request->linkedin_link,
+            'dribble_link'=> $request->dribble_link,
+            'pinterest_link'=> $request->pinterest_link,
+            'copywrite_text'=> $request->copywrite_text,
+            'created_at'=> Carbon::now()
+        ]);
+        return redirect()->route('footer.manage')->with('message','SuccessFully Inserted Footer');
     }
 
     /**
---
+++ resources/views/backend/pages/footer/FooterManage.blade.php
@@ -0,0 +1,67 @@
+@extends('backend.mastertemplate.masterAdminTemplate')
+@section('allbody')
+<div class="br-pagetitle">
+        <i class="icon ion-ios-home-outline"></i>
+        <div>
+          <h4>MANAGE About</h4>
+          <p class="mg-b-0">Do bigger things with Bracket plus, the responsive bootstrap 4 admin template.</p>
+        </div>
+        <div class="text-right">
+            <a href="{{route('dashboard')}}" class="btn btn-sm btn-success">Dashboard</a> /
+            <a href="{{route('about.add')}}" class="btn btn-sm btn-success">About add</a> 
+        </div>
+      </div>
+
+      <div class="br-pagebody">
+        <div class="row">
+        <div class="col-md-12">
+              <div class="card bg-white">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>TITLE</th>
+                                <th>S.Title</th>
+                                <th>S.Description</th>
+                                <th>L.Description</th>
+                                <th>Image</th>
+                                <th>status</th>
+                                <th>ACTION</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php $serial=1 @endphp
+                            @foreach($aboutall as $about)
+                            <tr>
+                                <td>{{$serial}}</td>
+                                <td>{{$about->title}}</td>
+                                <td>{{$about->short_title}}</td>
+                                <td>{{$about->short_description}}</td>
+                                <td>{!! $about->long_description !!}</td>
+                                <td>
+                                    <img width="100" src="{{asset('backend/aboutImage/'.$about->about_image)}}" alt="SLIDERIMAGE">    
+                               </td>
+                                <td>
+                                    @if($about->status == 1)
+                                     <span class="badge badge-info">Active</span>
+                                     @else
+                                     <span class="badge badge-warning">Inactive</span>
+                                     @endif
+                                    </td>
+                                <td>
+                                    <a href="{{route('edit.about',Crypt::encryptString($about->id))}}" class="btn-sm btn-info btn"><i class="fa fa-edit"></i></a>
+                                    <a href="{{route('about.delete',$about->id)}}" id="delete" class="btn-sm btn-danger btn"><i class="fa fa-trash"></i></a>
+                                    <a href="{{route('about.multiimage',$about->id)}}" class="btn-sm btn-dark btn"><i class="fa fa-plus"></i></a>
+                                </td>
+                            </tr>
+                            @php $serial++ @endphp
+                            @endforeach
+                        </tbody>
+                    </table>
+                        
+                   
+              </div><!-- card -->
+            </div>
+         </div>
+      </div>
+@endsection
